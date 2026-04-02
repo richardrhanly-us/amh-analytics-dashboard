407a15ef498a61d5958bd14f1904d8445b8f0af3
Richard Hanly <richardrhanly@gmail.com>
2026-04-01 21:12:54 -0500

    Update run_update_and_push.bat

--- run_update_and_push.bat
+++ run_update_and_push.bat
@@ -15,8 +15,14 @@ if errorlevel 1 (
 echo Adding app + gitignore + processed files...
 git add .gitignore
 git add src\app.py
+git add src\data_loader.py
+git add scripts\run_pipeline.py
+git add scripts\parse_checkins.py
+git add scripts\parse_rejects.py
 git add data\processed\checkins_clean.csv
 git add data\processed\rejects_clean.csv
+git add data\processed\checkins_history.csv
+git add data\processed\rejects_history.csv
 git add data\processed\pipeline_status.json
 
 echo Checking for staged changes...
