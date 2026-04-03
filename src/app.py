84d05d1236e001cec11a7750e4e163a3c7eba23b
Richard Hanly <richardrhanly@gmail.com>
2026-04-02 22:45:21 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1463,10 +1463,9 @@ AMH time = {avg_daily_checkins:,.1f} ÷ {AMH_RATE:.1f}
 
 #### Time Saved
 
-**Time saved = Manual time − AMH time**
+Time saved = Manual time − AMH time
+
 Time saved = {avg_daily_manual_hours:,.2f} − {avg_daily_amh_hours:,.2f}
-
-
 
 **Average Daily Staff time saved = {avg_saved:,.2f} hours**
 
