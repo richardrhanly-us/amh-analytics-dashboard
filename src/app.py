c9d7780a480982f0fad578e32a2ee3552fb9ea9e
Richard Hanly <richardrhanly@gmail.com>
2026-04-06 14:10:09 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1472,7 +1472,7 @@ if selected_view == "Live Today":
                         margin-top:3px;
                         line-height:1.1;
                     ">
-                        <span></span>
+                        <span>Slow</span>
                         <span style="font-weight:600; color:#4b5563;">{activity_label}</span>
                         <span>Busy</span>
                     </div>
