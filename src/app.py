af2f6f41c1f2f825ab1ac2bd1f2f6ff36e305a9b
Richard Hanly <richardrhanly@gmail.com>
2026-04-13 11:23:00 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -310,11 +310,6 @@ if auto_refresh_triggered:
     st.session_state["last_refresh_count"] = refresh_count
 
 
-st.write("selected_org_slug:", selected_org_slug)
-st.write("selected_branch_slug:", selected_branch_slug)
-st.write("selected_customer_id:", selected_customer_id)
-st.write("selected_branch_id:", selected_branch_id)
-
 pipeline_status = load_pipeline_status(
     org_slug=selected_customer_id,
     branch_slug=selected_branch_id,
